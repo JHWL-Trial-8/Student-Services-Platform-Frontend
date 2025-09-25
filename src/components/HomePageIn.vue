91e69901e8756768f3501729ea29f43a477cad66
ZestfulYK <717056060@qq.com>
2025-09-25 16:44:30 +0800

    feat：加入了LogPage和HomePageIn的注释 fix：修正了登录页面的密码输入框格式不正确的问题

--- src/components/HomePageIn.vue
+++ src/components/HomePageIn.vue
@@ -1,4 +1,4 @@
-<template>
+<template><!--与上一个相似，只是边栏可以页面跳转-->
     <div class="h-full w-full bg-gray-50 flex flex-col">
         <header class="bg-white shadow-sm py-4 px-6 h-20 w-full">
             <div class="flex items-center justify-between">
@@ -18,24 +18,28 @@
             <div class="flex flex-col w-60 min-h-screen p-10 bg-white 
                     border-red-400 border-t-2 rounded-lg shadow-lg">
                 <router-link to="/user" class="py-4 my-2 text-center font-semibold">个人信息</router-link>
+                <!--跳转到“/user”个人信息界面-->
                 <hr>
                 <router-link to="/up" class="py-4 my-2 text-center font-semibold">提交问题反馈</router-link>
+                <!--跳转到“/up”上传问题反馈界面-->
                 <hr>
                 <router-link to="/controlluser" class="py-4 my-2 text-center font-semibold" v-if="role === 'superadmin'">用户账号管理</router-link>
-                <hr>
+                <hr><!--跳转到“/controlluser”用户账号管理界面-->
                 <router-link to="/feedbackpage" class="py-4 my-2 text-center font-semibold" v-if="role === 'admin'">处理信息</router-link>
                 <router-link to="/feedbackpage" class="py-4 my-2 text-center font-semibold" v-if="role === 'superadmin'">处理信息</router-link>
+                <!--跳转到处理信息界面，不会合起来写，于是分开了写两个，但是功能不变，就是给管理员和超管显示这个选项-->
                 <hr>
                 <div class="w-auto rounded-md p-4"><!--信息显示-->
-                <img src="../assets/JHWL-Trial-8.jpg" alt="头像" class="flex">
+                <img src="../assets/JHWL-Trial-8.jpg" alt="头像" class="flex"><!--用户头像-->
                 </div>
-                <div class="py-4 my-2 text-center font-semibold">
+                <div class="py-4 my-2 text-center font-semibold"><!--用户名称-->
                     {{username}}
                 </div>
                 </div>
                 </div>
                 <div class="flex-1 p-10">
                 <div class="h-auto w-auto bg-white rounded-lg shadow-lg"></div>
+                <!--主体内容，以后可能会加入反馈的历史记录及通知-->
             </div>
         </div>
         <PageFoot></PageFoot>
@@ -46,12 +50,12 @@
     export default{
         data(){
             return{
-                username:localStorage.getItem('username'),
+                username:localStorage.getItem('username'),//读取用户信息及身份
                 role:localStorage.getItem('role')
             };
         },
         components:{
-            PageFoot
+            PageFoot//脚标导入，虽然只有一行但还是想这么写
         }
     }
 </script>
--- src/components/LogPage.vue
+++ src/components/LogPage.vue
@@ -1,33 +1,33 @@
 <template>
     <div class="flex flex-col min-h-screen">
-        <PageHead></PageHead>
-        <div class="flex-1 flex flex-row-reverse overflow-hidden">
+        <PageHead></PageHead><!--页面头,但是页面头点击是到/home页面-->
+        <div class="flex-1 flex flex-row-reverse overflow-hidden"><!--可以考虑加上侧栏，但是为什么登录页面会要侧栏呢-->
             <div class="flex-1 flex justify-end overflow-hidden">
             <div class=" p-32">
-                <form @keyup.enter="login()" @submit.prevent></form>
+                <form @keyup.enter="login()" @submit.prevent></form><!--需要加入一个回车登录的功能，还在研究中-->
                 <div class="flex flex-col w-96 h-full p-10 bg-white
-                        border-red-400 border-t-2 rounded-lg shadow-lg">
-                    <div class="py-4 my-2 text-center font-semibold text-4xl">登录</div>
+                        border-red-400 border-t-2 rounded-lg shadow-lg"><!--登录框，其实是由侧栏的代码改过来的-->
+                    <div class="py-4 my-2 text-center font-semibold text-4xl">登录</div><!--登录框标题-->
                     <div class="flex py-4">
                         <div class=" h-10 w-80 py-1 rounded shadow-sm flex justify-center items-center">
-                            <div class="p-1">邮箱:</div>
+                            <div class="p-1">邮箱:</div><!--邮箱输入框，但是需要一个能判断邮箱格式是否合法的函数并做出不同的显示-->
                             <input v-model="Email" type="text" class="flex-row p-1 border focus:outline-none focus:ring-2 focus:ring-blue-600 rounded-sm"
                             placeholder="user@example.com"  @blur="validateEmail">
                         </div>
                     </div>
                     <div class="flex py-4">
                         <div class="h-10 w-80 py-1 rounded shadow-sm flex justify-center items-center">
-                            <div class="p-1">密码:   </div>
-                            <input type="text" class="flex-row p-1 border focus:outline-none focus:ring-2 focus:ring-blue-600 rounded-sm"
+                            <div class="p-1">密码:   </div><!--密码输入框，刚改正密码输入类型不是password的问题-->
+                            <input type="password" class="flex-row p-1 border focus:outline-none focus:ring-2 focus:ring-blue-600 rounded-sm"
                             placeholder="Password" v-model="Password">
                         </div>
                     </div>
                     <div class="flex  justify-center items-center h-11">
                         <button class=" bg-red-500  text-black hover:bg-red-700 hover:text-white 
-                            rounded-sm w-16 h-8 shadow-sm" @click="login()">登录</button>
+                            rounded-sm w-16 h-8 shadow-sm" @click="login()">登录</button><!--登录按钮，绑定了登录事件login-->
                         <div class="w-16 h-8"></div>
                         <button class=" bg-red-500  text-black hover:bg-red-700 hover:text-white 
-                            rounded-sm w-16 h-8 shadow-sm " @click="register()">注册</button>
+                            rounded-sm w-16 h-8 shadow-sm " @click="register()">注册</button><!--注册按钮，绑定了页面跳转事件register-->
                     </div>
                 </div>
             </div>
@@ -35,14 +35,14 @@
         <div class="flex-1 flex justify-end">
             <div class="px-10 py-32">
                 <div class="flex flex-col">
-                    <img src="../assets/JHWL-Trial-8.jpg" alt="picture">
+                    <img src="../assets/JHWL-Trial-8.jpg" alt="picture"><!--左侧大照片-->
                 </div>
             </div>
         </div>
         </div>
     </div>
     <footer class="text-center py-4 bg-white border-t border-gray-200 flex-shrink-0">JHWL-Trial-8制作
-    </footer>
+    </footer><!--脚标-->
 </template>
 <script>
     import PageHead from './PageHead.vue';
@@ -58,14 +58,14 @@
             }
         },
         methods:{
-            login(){
+            login(){//随便生成的几个参数，之后要用axios查询参数
                 localStorage.setItem('username','username');
                 localStorage.setItem('access_token','111');
                 localStorage.setItem('access_type','admin');
                 this.$router.push('/home');
             },
             register(){
-                this.$router.push('/register')
+                this.$router.push('/register')//跳传到/register注册页面
             }
         }
     }
